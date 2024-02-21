46555a5f0bd69eacbb6e79473271a2ee97868c0e
hopez13 <140780838+hopez13@users.noreply.github.com>
2024-02-21 09:10:15 +0530

    Update ocr.sh

--- ocr.sh
+++ ocr.sh
@@ -8,10 +8,21 @@ log() {
 
 URL="https://archive.org/download/the-srimad-devi-bhagawatam-swami-vijnanananda/The%20Srimad%20Devi%20Bhagawatam%20-%20Swami%20Vijnanananda.pdf"
 
-curl -o ocr.pdf "$URL" && log "INFO" "Successfully downloaded the PDF file from $URL" || { log "ERROR" "Could not download the PDF file from $URL. Status code: $?" ; exit 1 ; }
+wget -O ocr.pdf "$URL"
+if [ $? -eq 0 ]; then
+  log "INFO" "Successfully downloaded the PDF file from $URL\n"
+else
+  log "ERROR" "Could not download the PDF file from $URL. Status code: $?\n"
+  exit 1
+fi
 
-pdfimages -png -f 1 -l 10 ocr.pdf ocr && log "INFO" "Successfully extracted images from the PDF file" || { log "ERROR" "Could not extract images from the PDF file. Status code: $?" ; exit 1 ; }
-
+pdfimages -png -f 1 -l 10 ocr.pdf ocr
+if [ $? -eq 0 ]; then
+  log "INFO" "Successfully extracted images from the PDF file\n"
+else
+  log "ERROR" "Could not extract images from the PDF file. Status code: $?\n"
+  exit 1
+fi
 
 touch output.pdf
 
@@ -19,19 +30,18 @@ for image in ocr*.png; do
   page=${image#ocr-}
   page=${page%.png}
 
-  log "INFO" "Processing page $page"
+  log "INFO" "Processing page $page\n"
 
   tesseract "$image" "$image" -l eng pdf
 
-  status=$?
-  if [ $status -eq 0 ]; then
-    log "INFO" "Finished converting page $page to PDF"
+  if [ $? -eq 0 ]; then
+    log "INFO" "Finished converting page $page to PDF\n"
   else
-    log "ERROR" "Could not convert page $page to PDF. Status code: $status"
+    log "ERROR" "Could not convert page $page to PDF. Status code: $?\n"
     exit 1
   fi
 
   pdftk output.pdf "$image".pdf cat output output.pdf
 done
 
-log "INFO" "Saved the output PDF file as output.pdf"
+log "INFO" "Saved the output PDF file as output.pdf\n"
